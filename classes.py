2df5e94f75249d8c2a22d64b50c7fe316a43525c
Walter <dejonghwalterh@sau.edu>
2024-05-26 19:29:57 +0200

    Added more classes

--- classes.py
+++ classes.py
@@ -21,3 +21,49 @@ class Ace(Card):
     def max_value(self) -> int:
         self._value = 10
         return self._value
+
+
+class Deck():
+    def __init__(self) -> None:
+        pass
+
+    def deal(self) -> Card:
+        pass
+
+    def shuffle(self) -> None:
+        pass
+
+    @property
+    def cards(self) -> set[Card]:
+        pass
+
+
+class Player():
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def name(self) -> str:
+        pass
+
+    @property
+    def cards(self) -> set[Card]:
+        pass
+
+    @property
+    def hand(self) -> int:
+        pass
+
+    @property
+    def money(self) -> int:
+        pass
+
+    @money.setter
+    def money(self, amount: int) -> None:
+        '''Adds the specified amount to the player's balance.'''
+
+        pass
+
+    def bet(self, amount: int) -> int:
+        '''Withdraws an amount from the players money to wager'''
+        pass
